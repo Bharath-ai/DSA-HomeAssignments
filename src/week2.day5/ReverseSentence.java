511da74ca3a133e33facf22e2909688431c16953
Bharath-ai <bharathece15@gmail.com>
2023-05-14 17:16:15 +0530

    Week2 Programs

---
+++ src/week2.day5/ReverseSentence.java
@@ -0,0 +1,40 @@
+package week2.day5;
+
+import org.junit.Test;
+
+public class ReverseSentence {
+
+	/*
+	 * Given a string s, reverse the order of characters in each word within a
+	 * sentence while still preserving whitespace and initial word order.
+	 */
+
+	@Test
+	public void example() {
+		String s = "Let's take LeetCode contest";
+		reverseSentence(s);
+	}
+
+	private void reverseSentence(String s) {
+		String[] words = s.split(" ");
+		String output = "";
+
+		char temp = ' ';
+		for (String word : words) {
+			int left = 0;
+			int right = word.length()-1;
+			char[] ch = word.toCharArray();
+			while (left < right) {
+				temp = ch[right];
+				ch[right--] = ch[left];
+				ch[left++] = temp;
+			}
+
+			output = output+" "+String.valueOf(ch).trim();
+		}
+
+		System.out.println(output);
+
+	}
+
+}
